27692cc8228e97d5bf091c7e1b94f93d17efc4b3
ronyaburaihan <ronyaburaihan@gmail.com>
2023-07-31 10:56:51 +0600

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Abu Raihan Rony on 31/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
