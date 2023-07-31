f4081b9766d67f6b4a6d02e8b720b13f0460dd32
ronyaburaihan <ronyaburaihan@gmail.com>
2023-07-31 22:28:34 +0600

    Color extension added

---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Abu Raihan Rony on 31/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("ColorRed")
+    let secondaryText = Color("SecondaryTextColor")
+}
