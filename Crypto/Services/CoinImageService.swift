91bab6005fcf45d02c13d05924fce29b2489e187
ronyaburaihan <ronyaburaihan@gmail.com>
2023-08-04 12:07:28 +0600

    Saving images to the File Manager & retrieving from it

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -15,13 +15,27 @@ class CoinImageService {
     
     var imageSubscription: AnyCancellable?
     private let coin: Coin
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: Coin) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrived image from File Manager!")
+        } else {
+            dowloadCoinImage()
+            print("Download image now")
+        }
+    }
+    
+    private func dowloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkManager.loadDataFromAPI(url: url)
@@ -29,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                 self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
              })
     }
     
