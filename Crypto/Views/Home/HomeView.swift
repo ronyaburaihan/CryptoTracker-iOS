4a140ef6fbd5b691972757b357e801b98b31a3c1
ronyaburaihan <ronyaburaihan@gmail.com>
2023-10-16 16:01:27 +0600

    Settings screen added using a List with Sections

--- Crypto/Views/Home/HomeView.swift
+++ Crypto/Views/Home/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
     @EnvironmentObject private var viewModel: HomeViewModel
     @State private var showPortfolio: Bool = false  // animate right
     @State private var showPortfolioView: Bool = false // new sheet
-    
+    @State private var showSettingsView: Bool = false // new sheet
     @State private var selectedCoin: Coin? = nil
     @State private var showDetailView: Bool = false
     
@@ -49,6 +49,9 @@ struct HomeView: View {
                 
                 Spacer(minLength: 0)
             }
+            .sheet(isPresented: $showSettingsView, content: {
+                SettingsView()
+            })
         }
         .background(
             NavigationLink(
@@ -78,6 +81,8 @@ extension HomeView {
                 .onTapGesture {
                     if showPortfolio {
                         showPortfolioView.toggle()
+                    } else {
+                        showSettingsView.toggle()
                     }
                 }
                 .background(CircleButtonAnimation(animate: $showPortfolio))
---
+++ Crypto/Views/Settings/SettingsView.swift
@@ -0,0 +1,83 @@
+//
+//  SettingsView.swift
+//  Crypto
+//
+//  Created by Abu Raihan Rony on 16/10/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    let defaultUrl = URL(string: "https://www.google.com")!
+    let coffeUrl = URL(string: "https://www.buymeacoffee.com/ronyaburaihan")!
+    let coingekoURL = URL(string: "https://www.coingecko.com")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                cryptoTrackerSection
+                 coinGeckoSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    CrossButton()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
+
+extension SettingsView {
+    
+    private var cryptoTrackerSection: some View {
+        Section(header: Text("Crypto tracker")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+                
+            }
+            .padding(.vertical)
+            
+            Link("Google", destination: defaultUrl)
+            Link("Buy me a coffee", destination: coffeUrl)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+                
+            }
+            .padding(.vertical)
+            
+            Link("Visit CoinGecko", destination: coingekoURL)
+            Link("Buy me a coffee", destination: coffeUrl)
+        }
+    }
+}
