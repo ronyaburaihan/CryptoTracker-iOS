82caf23d05acbb078d1e8d13ffdcd8881c1ef53f
ronyaburaihan <ronyaburaihan@gmail.com>
2023-10-16 11:59:38 +0600

    Custom line chart added to display historical price data

---
+++ Crypto/Extensions/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  Crypto
+//
+//  Created by Abu Raihan Rony on 16/10/23.
+//
+
+import Foundation
---
+++ Crypto/Views/Detail/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  Crypto
+//
+//  Created by Abu Raihan Rony on 16/10/23.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
